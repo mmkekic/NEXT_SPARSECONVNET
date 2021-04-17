e691bd019a821ced6fdd07e4b379d171eaf56581
mmkekic <mmkekic@gmail.com>
2021-04-17 22:29:50 +0200

    Add new fixtures to conftest

--- next_sparseconvnet/conftest.py
+++ next_sparseconvnet/conftest.py
@@ -1,6 +1,20 @@
 import pytest
+import os
 import pandas as pd
 
 @pytest.fixture(scope = 'session')
 def labels_df():
     return pd.read_csv('./test_files/cut_labels.cvs')
+
+@pytest.fixture(scope = 'session')
+def tmpdir(tmpdir_factory):
+    return tmpdir_factory.mktemp('output_files')
+
+
+@pytest.fixture(scope = 'session')
+def TEST_DATA():
+    return os.environ['TEST_DATA']
+
+@pytest.fixture(scope = 'session')
+def MCdataset(TEST_DATA):
+    return os.path.join(TEST_DATA, "MC_dataset.h5")
