709c450aa1e9d10982be6d3bded7623102eed996
martinperezmaneiro <martinperezmaneiro@gmail.com>
2021-05-07 12:44:21 +0200

    Add test for train and validation functions

--- next_sparseconvnet/utils/train_utils_test.py
+++ next_sparseconvnet/utils/train_utils_test.py
@@ -14,3 +14,46 @@ def test_IoU():
     n = 5
     iou = IoU(a, b, nclass = n)
     assert len(iou) == n
+
+
+
+def test_train_one_epoch_segmentation(MCdataset):
+    datagen = DataGen(MCdataset, LabelType.Segmentation, nevents = 3)
+    loader = torch.utils.data.DataLoader(datagen, batch_size = 1, shuffle = True, num_workers=1, collate_fn=collatefn, drop_last=True, pin_memory=False)
+
+    spatial_size = (51, 51, 51)
+    init_conv_nplanes = 4
+    init_conv_kernel = 3
+    kernel_sizes = [7, 5, 3]
+    stride_sizes = [2, 2]
+    basic_num = 3
+    nclasses = 3
+
+    net = UNet(spatial_size, init_conv_nplanes, init_conv_kernel, kernel_sizes, stride_sizes, basic_num, nclasses = nclasses)
+    net = net.cuda()
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-2, betas=(0.9, 0.999), eps=1e-6, weight_decay=0)
+
+    train_one_epoch_segmentation(0, net, criterion, optimizer, loader)
+
+
+
+def test_valid_one_epoch_segmentation(MCdataset):
+    datagen = DataGen(MCdataset, LabelType.Segmentation, nevents = 3)
+    loader = torch.utils.data.DataLoader(datagen, batch_size = 1, shuffle = True, num_workers=1, collate_fn=collatefn, drop_last=True, pin_memory=False)
+
+    spatial_size = (51, 51, 51)
+    init_conv_nplanes = 4
+    init_conv_kernel = 3
+    kernel_sizes = [7, 5, 3]
+    stride_sizes = [2, 2]
+    basic_num = 3
+    nclasses = 3
+
+    net = UNet(spatial_size, init_conv_nplanes, init_conv_kernel, kernel_sizes, stride_sizes, basic_num, nclasses = nclasses)
+    net = net.cuda()
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    valid_one_epoch_segmentation(net, criterion, loader)
