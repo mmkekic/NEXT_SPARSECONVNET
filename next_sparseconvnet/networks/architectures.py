267833328a1b9813c1a4c9266ac1434077c7e4f5
juliolopezcarballo <jlcjuli2@gmail.com>
2021-05-07 10:39:01 +0200

    Change PR Comments

--- next_sparseconvnet/networks/architectures.py
+++ next_sparseconvnet/networks/architectures.py
@@ -148,52 +148,23 @@ class UNet(torch.nn.Module):
 
 class ResNet(torch.nn.Module):
     '''
-    This class implements a net structure built with ResNet blocks. It takes a tuple of (coordinates, features)
-    and passes it through the ResNet.
+        This class implements a net structure built with ResNet blocks. It takes a tuple of (coordinates, features)
+        and passes it through the ResNet.
 
-    ...
+        ...
 
-    Attributes
-    ----------
-    spatial_size : tuple
-        The spatial size of the input layer. Size of the tuple is also the dimension.
-    inplanes : int
-        Number of planes we want after the initial SubmanifoldConvolution, that is, to begin downsampling.
-    kernel : list
-        Size of the kernels applied in each layer or block. Its length also indicates the level depth of the net.
-    stride : int
-        Applied stride in every layer or block.
-    conv_kernel : int
-        Kernel for the first convolutional layer.
-    basic_num : int
-        Number of times a basic residual block is passed in each level.
-    nclasses : int, optional
-        Number of output classes for predictions. The default is 2.
-    dim : int, optional
-        Number of dimensions of the input. The default is 3.
-    start_planes : int, optional
-        Number of planes that enter the ResNet. The default is 1.
-
-    Methods
-    -------
-    forward(x)
-        Passes the input through the ResNet
-    '''
-    def __init__(self, spatial_size, init_conv_nplanes, init_conv_kernel, kernel_sizes, stride, basic_num, nclasses = 2, dim = 3, start_planes = 1):
-        '''
-        Parameters
+        Attributes
         ----------
         spatial_size : tuple
-            The spatial size of the input layer. Size of the tuple is also the dimension.
-        init_conv_nplaness : int
+        The spatial size of the input layer. Size of the tuple is also the dimension.
+        inplanes : int
             Number of planes we want after the initial SubmanifoldConvolution, that is, to begin downsampling.
-        init_conv_kernel : int
-            Kernel for the first convolutional layer.
-        kernel_sizes : list
+        kernel : list
             Size of the kernels applied in each layer or block. Its length also indicates the level depth of the net.
-            Last element corresponds just to the kernel of the basic block in the bottom of the net.
         stride : int
             Applied stride in every layer or block.
+        conv_kernel : int
+            Kernel for the first convolutional layer.
         basic_num : int
             Number of times a basic residual block is passed in each level.
         nclasses : int, optional
@@ -201,7 +172,37 @@ class ResNet(torch.nn.Module):
         dim : int, optional
             Number of dimensions of the input. The default is 3.
         start_planes : int, optional
-            Number of planes that enter the net. The default is 1.
+            Number of planes that enter the ResNet. The default is 1.
+
+        Methods
+        -------
+        forward(x)
+            Passes the input through the ResNet
+    '''
+    def __init__(self, spatial_size, init_conv_nplanes, init_conv_kernel, kernel_sizes, stride_sizes, basic_num, nclasses = 2, dim = 3, start_planes = 1):
+        '''
+            Parameters
+            ----------
+            spatial_size : tuple
+                The spatial size of the input layer. Size of the tuple is also the dimension.
+            init_conv_nplaness : int
+                Number of planes we want after the initial SubmanifoldConvolution, that is, to begin downsampling.
+            init_conv_kernel : int
+                Kernel for the first convolutional layer.
+            kernel_sizes : list
+                Size of the kernels applied in each layer or block. Its length also indicates the level depth of the net.
+                Last element corresponds just to the kernel of the basic block in the bottom of the net.
+            stride_sizes : list
+                Sizes of the kernels applied in each layer or block. Its length also indicates the level depth of the net.
+                Last element corresponds just to the kernel of the basic block in the bottom of the net.
+            basic_num : int
+                Number of times a basic residual block is passed in each level.
+            nclasses : int, optional
+                Number of output classes for predictions. The default is 2.
+            dim : int, optional
+                Number of dimensions of the input. The default is 3.
+            start_planes : int, optional
+                Number of planes that enter the net. The default is 1.
         '''
         torch.nn.Module.__init__(self)
 
@@ -211,9 +212,6 @@ class ResNet(torch.nn.Module):
         self.inp     = scn.InputLayer(dim, spatial_size)
         self.convBN  = ConvBNBlock(start_planes, init_conv_nplanes, init_conv_kernel)
         inplanes     = init_conv_nplanes
-        self.max     = scn.MaxPooling(dim,2,2)
-        self.sparse  = scn.SparseToDense(dim,inplanes)
-        self.linear  = torch.nn.Linear(inplanes, nclasses)
 
         self.downsample = torch.nn.ModuleList([])
         self.basic      = torch.nn.ModuleList([torch.nn.ModuleList([]) for i in range(self.level_depth - 1)])
@@ -223,24 +221,32 @@ class ResNet(torch.nn.Module):
             for j in range(basic_num):
                 self.basic[i].append(ResidualBlock_basic(inplanes, kernel_sizes[i]))
 
-                self.downsample.append(ResidualBlock_downsample(inplanes, kernel_sizes[i], stride))
+            self.downsample.append(ResidualBlock_downsample(inplanes, kernel_sizes[i], stride_sizes[i]))
 
-                inplanes = inplanes * 2
+            inplanes = inplanes * 2
+
+            for j in range(basic_num):
+                self.bottom.append(ResidualBlock_basic(inplanes, kernel_sizes[-1]))
+
+        self.max     = scn.MaxPooling(dim,max(calculate_output_dimension(spatial_size, kernel_sizes, stride_sizes)),1)
+        self.sparse  = scn.SparseToDense(dim,inplanes)
+        self.linear1 = torch.nn.Linear(inplanes, 32, nclasses)
+        self.linear2 = torch.nn.Linear(32, 1, nclasses)
 
     def forward(self, x):
     '''
-    Passes x through the net.
+        Passes x through the net.
 
-    Parameters
-    ----------
-    x : tuple
-        It takes a tuple with (coord, features), where coord is a torch tensor with size [features_number, batch_size],
-        and features is another torch tensor, with size [features_number, start_planes]
+        Parameters
+        ----------
+        x : tuple
+            It takes a tuple with (coord, features), where coord is a torch tensor with size [features_number, batch_size],
+            and features is another torch tensor, with size [features_number, start_planes]
 
-    Returns
-    -------
-    x : torch.Tensor
-        A tensor with size [features_number, nclasses].
+        Returns
+        -------
+        x : torch.Tensor
+            A tensor with size [features_number, nclasses].
 
     '''
     x = self.inp(x)
@@ -256,6 +262,8 @@ class ResNet(torch.nn.Module):
 
     x = self.max(x)
     x = self.sparse(x)
-    x = self.linear(x)
+    x = x.squeeze()
+    x = self.linear1(x)
+    x = self.linear2(x)
 
     return x
