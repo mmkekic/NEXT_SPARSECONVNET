0d304f9f8bc41fbff16cec918961777ac849e442
martinperezmaneiro <martinperezmaneiro@gmail.com>
2021-05-18 20:48:15 +0200

    Add full train to the script

--- scripts/main.py
+++ scripts/main.py
@@ -7,11 +7,13 @@ To be called as main.py -conf conf_filename -a train
 
 import os
 import copy
+import torch
 from argparse     import ArgumentParser
 from argparse     import Namespace
 
-from next_sparseconvnet.utils.data_loaders     import LabelType
-from next_sparseconvnet.networks.architectures import NetArchitecture
+from next_sparseconvnet.utils.data_loaders     import * #LabelType
+from next_sparseconvnet.networks.architectures import * #NetArchitecture
+from next_sparseconvnet.utils.train_utils      import *
 
 def is_valid_action(parser, arg):
     if not arg in ['train']:#, 'predict']:
@@ -55,6 +57,37 @@ if __name__ == '__main__':
 
     # make sure LabelType and NetArchitectures are consistent
     # construct the network
+    if parameters.netarch == NetArchitecture.UNet:
+        net = UNet(parameters.spatial_size,
+                   parameters.init_conv_nplanes,
+                   parameters.init_conv_kernel,
+                   parameters.kernel_sizes,
+                   parameters.stride_sizes,
+                   parameters.basic_num,
+                   momentum = parameters.momentum)
+        net = net.cuda()
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(),
+                                 lr = parameters.lr,
+                                 betas = parameters.betas,
+                                 eps = parameters.eps,
+                                 weight_decay = parameters.weight_decay)
+
     # if action is train:
-        #train netowrk
+    if action == 'train':
+        train_segmentation(nepoch = parameters.nepoch,
+                           train_data_path = parameters.train_file,
+                           valid_data_path = parameters.valid_file,
+                           train_batch_size = parameters.train_batch,
+                           valid_batch_size = parameters.valid_batch,
+                           net = net,
+                           criterion = criterion,
+                           optimizer = optimizer,
+                           checkpoint_dir = parameters.checkpoint_dir,
+                           tensorboard_dir = parameters.tensorboard_dir,
+                           num_workers = parameters.num_workers,
+                           nevents_train = parameters.nevents_train,
+                           nevents_valid = parameters.nevents_valid)
+
     # else if prediction : still not implemented
